69416c2b390a02963a84ed7656e9fc959fa853ab
jeppemannen <42612862+jeppemannen@users.noreply.github.com>
2023-06-04 17:22:30 +0200

    Third

--- gkgkinv/src/App.tsx
+++ gkgkinv/src/App.tsx
@@ -2,22 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+ 
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+      <p>
+          GKGK-Invitational
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <img src="https://scontent.fosl4-2.fna.fbcdn.net/v/t1.15752-9/297049970_948898449841581_3182713427378061223_n.jpg?_nc_cat=100&cb=99be929b-59f725be&ccb=1-7&_nc_sid=ae9488&_nc_ohc=uXwXhUGOUAcAX8mkH6J&_nc_ht=scontent.fosl4-2.fna&oh=03_AdSeJvO1eROiSIPVsyG0RYpyWH228f8pA-gtVxL6Djg1fw&oe=64A42D3C"
+          height="500px"/>
+        </p>
+
+        <p>
+          Dato: TBA
+        </p>
+        <p>
+          Turneringsformat: Scramble
+        </p>
+        <p>
+        
+          Stad: Overvoll Golfbane
+        </p>
       </header>
     </div>
   );
